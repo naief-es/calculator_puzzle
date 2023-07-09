ceb806f5e5f4e585445e60d0880700466b453a2f
naief-es <121799670+naief-es@users.noreply.github.com>
2023-07-09 15:22:54 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,15 @@
+import pytest
+from calculator import Calculator,add,multiply
+"""module , function ,session, class"""
+@pytest.fixture(scope='session')
+def calculator():
+    """inctance from calc"""
+    return Calculator()
 
+@pytest.fixture(scope='session')
+def set_function_to_add(calculator):
+    calculator.set_function(add)
+
+@pytest.fixture(scope='session')
+def set_function_to_multiply(calculator):
+    calculator.set_function(multiply)
